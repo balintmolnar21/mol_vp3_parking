4d2ea399beb02ba2751d026cda565918c26e5125
Molnár Bálint <molnar.balint.otto@gmail.com>
2026-09-11 23:27:34 +0200

    Initialize mol_vp3_parking package

--- setup.py
+++ setup.py
@@ -2,28 +2,41 @@ from setuptools import find_packages, setup
 from glob import glob
 import os
 
+
 package_name = 'mol_vp3_parking'
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name),
+            glob('launch/*launch.[pxy][yma]*')
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='todo',
-    maintainer_email='todo@todo.com',
-    description='TODO: Package description',
+
+    maintainer='Molnar Balint',
+    maintainer_email='molnar.balint.otto@gmail.com',
+
+    description='ROS 2 based simulated parking assistant implemented in Python.',
     license='GNU General Public License v3.0',
+
     tests_require=['pytest'],
+
     entry_points={
         'console_scripts': [
-            # 'control_vehicle = mol_vp3_parking.control_vehicle:main',
         ],
     },
 )
